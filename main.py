f44b8074f6db73d37c0b2d8c91dfa37b5b48dee9
K-SEKYUN <81982425+K-SEKYUN@users.noreply.github.com>
2021-08-24 23:36:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def on_message(message):
             Text = Text + " " + learn[i]
         encText = Text
 
-        chromedriver_dir = r'chromedriver'
+        chromedriver_dir = os.path.join('chromedriver')
         driver = webdriver.Chrome(chromedriver_dir, options=options)
         driver.get('https://cse.google.com/cse?cx=010798177249342776914:8madl3htvdg&q=' + encText)
         source = driver.page_source
