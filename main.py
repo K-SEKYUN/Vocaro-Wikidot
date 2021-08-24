ec393f92cad76933ea4d461609dac5ee0ca17b9d
K-SEKYUN <81982425+K-SEKYUN@users.noreply.github.com>
2021-08-25 00:40:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ client = discord.Client()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
 
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
